2f4bd5b7490cae6d341f19fc6340f41eba2e24fa
Nick Gilbert <nick.gilbert@worldvision.org.uk>
2020-02-15 11:00:31 +0000

    Removed redundant code in the JSON web token manager and changed the secret value

--- LoginServiceDemo/Tokens/JsonWebTokenManager.cs
+++ LoginServiceDemo/Tokens/JsonWebTokenManager.cs
@@ -15,7 +15,7 @@ namespace LoginServiceDemo.Models
     {
         // HMACSHA256 generated secret key required for encryption and decryption
         // To do: store in the Key Vault for Azure implementation
-        private static string Secret = "XCAP05H6LoKvbRRa/QkqLNMI7cOHguaRyHzyg7n5qEkGjQmtBhz4SzYh4Fqwjyi3KJHlSXKPwVu2+bXr6CtpgQ==";
+        private static string Secret = "ALLWORKANDNOPLAYMAKESJACKADULLBOY123";
 
         // Token genaration method where a token is created and a string version of it returned
         public static string GenerateToken(string username)
@@ -25,7 +25,7 @@ namespace LoginServiceDemo.Models
             /// Creates the descriptor object, which represents the main content of the JWT, 
             /// such as the claims, the expiration date and the signing information. 
             /// The token is created and a string version of it is returned. In this example
-            /// only the username claim is added but the list of claim types that can be added is 
+            /// only the username is added but the list of claim types that can be added is 
             /// large. 
             /// It is also possible to add custom data to the token post creation by accessing the JWT
             /// payload property with code like: token.Payload["NicksFavouriteFood"] = "Huel";
@@ -37,7 +37,7 @@ namespace LoginServiceDemo.Models
             {
                 Subject = new ClaimsIdentity(new[] {
                       new Claim(ClaimTypes.Name, username)}),
-                Expires = DateTime.UtcNow.AddMinutes(60),
+                Expires = DateTime.UtcNow.AddMinutes(5),
                 SigningCredentials = new SigningCredentials(securityKey,
                 SecurityAlgorithms.HmacSha256Signature)
             };
@@ -45,69 +45,8 @@ namespace LoginServiceDemo.Models
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
             JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
             return handler.WriteToken(token);
-        }
+        } 
 
-        // The method reads the token in string format and converts it into a JwtSecurityToken
-        public static ClaimsPrincipal GetPrincipal(string token)
-        {
-            try
-            {
-                // Convert the token (in string format) to a JWT Security Token
-                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
-
-                // Bonb out if null
-                if (jwtToken == null)
-                {
-                    return null;
-                }
-                
-                // Generate parameters for use in the validation process and re-creates the key
-                // using the same secret as used in the token generation step
-                byte[] key = Convert.FromBase64String(Secret);
-                TokenValidationParameters parameters = new TokenValidationParameters()
-                {
-                    RequireExpirationTime = true,
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    IssuerSigningKey = new SymmetricSecurityKey(key)
-                };
-
-                SecurityToken securityToken;
-                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
-
-                return principal;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
-
-        // This method creates the Principal object using the token and then extracts the Identity object out of it.
-        public static string ValidateToken(string token)
-        {
-            string username = null;
-            ClaimsPrincipal principal = GetPrincipal(token);
-
-            if (principal == null)
-            {
-                return null;
-            }
-                
-            ClaimsIdentity identity = null;
-            try
-            {
-                identity = (ClaimsIdentity)principal.Identity;
-            }
-            catch (NullReferenceException)
-            {
-                return null;
-            }
-            Claim usernameClaim = identity.FindFirst(ClaimTypes.Name);
-            username = usernameClaim.Value;
-            return username;
-        }
     }
 
 }
